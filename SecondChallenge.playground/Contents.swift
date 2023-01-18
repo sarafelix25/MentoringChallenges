bafe3c05e383afd359801bc42ea467ea0d5cfa40
Sara Felix <sfelix@lifescan.com>
2023-01-18 16:45:00 -0300

    adjustments

--- SecondChallenge.playground/Contents.swift
+++ SecondChallenge.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-func averages(average: [Int]) -> Int {
-    let soma = average.reduce(0,+)
+func averages(average: [Int], number1: Int, number2: Int) -> Int {
+    let results = average.filter( { $0 < number1}).filter({ $0 > number2})
+        let soma = results.reduce(0,+)
     let quantities = average.count
     let media = soma/quantities
 
-    return media
-
+        return media
 }
 
-averages(average: [13, 22, 4, 39, 20, 45, 81])
+averages(average: [13, 32, 30, 29, 18, 51, 40, 79, 17], number1: 40, number2: 0)
