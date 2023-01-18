269b3f499362ab677a7bed6a64e10ac888bd06a2
Sara Felix <sfelix@lifescan.com>
2023-01-18 17:05:56 -0300

    adjustments final

--- SecondChallenge.playground/Contents.swift
+++ SecondChallenge.playground/Contents.swift
@@ -1,13 +1,20 @@
 import UIKit
 
-func averages(average: [Int], number1: Int, number2: Int) -> Int {
-    let results = average.filter({ $0 < number1})
-        .filter({ $0 > number2})
-    let soma = results.reduce(0,+)
-    let quantities = average.count
-    let media = soma/quantities
+/*
+Crie uma função que faça o cálculo da média com a separação das categorias.
+ - Média de pessoas com menos de 18 anos
+ - Média de pessoas entre 19 e 40 anos
+ - Média de pessoas acima de 41 anos
+ - Média de todas as idades
+ */
 
-    return media
+func calculateAverage(ages: [Int], number1: Int, number2: Int) -> Int {
+    let results = ages.filter({$0 < number1 && $0 > number2})
+    let soma = results.reduce(0,+)
+    let quantities = ages.count
+    let average = soma/quantities
+
+    return average
 }
 
-averages(average: [13, 32, 30, 29, 18, 51, 40, 79, 17], number1: 40, number2: 0)
+calculateAverage(ages: [13, 32, 30, 29, 18, 51, 40, 79, 17], number1: 32, number2: 0)
